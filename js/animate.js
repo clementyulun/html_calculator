86e47f60530ebecf603228d73718e08af20a5257
Picarico <Picarico@localhost>
2020-10-08 00:09:50 +0800

    refactor and add the useless animation

---
+++ js/animate.js
@@ -0,0 +1,12 @@
+function error_ani(){
+    document.getElementById("dis_cat").style.display = 'block';
+}
+
+function answer_ani(){
+    document.getElementById("ok_cat").style.display = 'block';
+    
+}
+
+function pic_close(el_str){
+    document.getElementById(el_str).style.display = 'none';
+}
